c1f2b6ab79e7253c4611e3673069a76bc75dc587
Rochagrsp <gabrielpereirarocha1710@gmail.com>
2026-05-18 20:53:03 -0300

    Sistema de chamado

--- index.py
+++ index.py
@@ -22,7 +22,26 @@ if st.button("Abrir Chamado"):
         }
         st.session_state.chamados.append(chamado)
         st.success("Chamado aberto com sucesso!")
+
+st.subheader("Lista de Chamados")
+if len(st.session_state.chamados) ==0:
+    st.warning("Nenhum chamado aberto!")
+else:
+    for i,chamado in enumerate (st.session_state.chamados):
+        st.write(f"{chamado['Título']}")
+        st.write(f"Descrição: {chamado['Descrição']}")
+        st.write(f"Status: {chamado['Status']}")
+
+#ALTERAR STATUS
+        novo_status = st.selectbox(
+            "Alterar Status",
+            ["Aberto","Em andamento","Finalizado"],
+            key=f"status{i}"
+        )
+        #BOTÃO PARA ALTERAR
+        if st.button("Atualizar Status", key=f"btn{i}"):
+            st.session_state.chamados[i]["Status"]=novo_status
+            st.success("Status Atualizado!")
+            st.rerun()
+        st.divider()
         
-
-
-
